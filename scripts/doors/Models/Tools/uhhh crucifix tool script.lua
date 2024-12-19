2828a1de765ab8710fca5ecddda865b87cf822b6
bamb421 <84636375+bamb421@users.noreply.github.com>
2024-12-19 19:31:44 +1300

    Create uhhh crucifix tool script.lua

---
+++ scripts/doors/Models/Tools/uhhh crucifix tool script.lua
@@ -0,0 +1,19 @@
+local vynixuModules = {
+	Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+}
+
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+
+local Crucifix = LoadCustomInstance("https://github.com/bamb421/bigpoop/blob/main/scripts/doors/Models/Tools/Crucifix.rbxm?raw=true")
+Crucifix.Parent = game.Players.LocalPlayer.Backpack
+
+local IdleAnimation = Crucifix.Animations.idle
+local IdleTrack = Character:FindFirstChild("Humanoid"):LoadAnimation(IdleAnimation)
+
+Crucifix.Equipped:Connect(function()
+	IdleTrack:Play()
+end)
+Crucifix.UnequippedEquipped:Connect(function()
+	IdleTrack:Stop()
+end)
